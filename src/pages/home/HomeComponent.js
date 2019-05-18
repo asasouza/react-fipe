9e31358b0deb0084656831d5b04c750aa6f0c744
Alex Sandro A. Souza <asasouza.alex@gmail.com>
2019-05-18 19:58:01 -0300

    Comunicação com a API
    1. Ajustado as actions e componente para realizar as requisições necessárias na API para exibir as informações corretas.
    2. Adicionado componente 'react-select' para utilizar como select das informações, tal componente foi escolhido devido sua facilidade de uso e features úteis como a pesquisa e demonstração de loading.

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -1,14 +1,74 @@
+// modules
 import React, { Component } from 'react';
+import Select from 'react-select';
+// store
 import { connect } from '../../stores';
 
 class HomeComponent extends Component {
 	render() {
 		console.log(this.props);
-		const { vehicleStore: { factory, setFactory } } = this.props;
+		const { vehicleStore: { 
+			brand,
+			brandList,
+			changeVehicleData, 
+			fipeInfo,
+			model,
+			modelList,
+			type, 
+			typeList,
+			year,
+			yearList
+		} } = this.props;
+		console.log(fipeInfo);
 		return (
 			<div>
-				<p>{factory}</p>
-				<button onClick={() => setFactory()}>Click</button>
+				<div>
+					<Select 
+						options={typeList}
+						value={type}
+						onChange={(value) => changeVehicleData('Type', value)}
+						placeholder='Selecione o tipo de veículo'
+					/>
+					<Select 
+						options={brandList === 'loading' ? [] : brandList}
+						isLoading={brandList === 'loading'}
+						loadingMessage={() => 'Carregando...'}
+						value={brand}
+						onChange={(value) => changeVehicleData('Brand', value)}
+						noOptionsMessage={() => 'Não há opções a serem selecionadas.'}
+						placeholder={brandList.length ? 'Selecione a marca do seu veículo' : 'Selecione um tipo de veículo para escolher'}
+					/>
+					<Select 
+						options={modelList === 'loading' ? [] : modelList}
+						isLoading={modelList === 'loading'}
+						loadingMessage={() => 'Carregando...'}
+						value={model}
+						onChange={(value) => changeVehicleData('Model', value)}
+						noOptionsMessage={() => 'Não há opções a serem selecionadas.'}
+						placeholder={modelList.length ? 'Selecione o modelo do seu veículo' : 'Selecione a marca do veículo para escolher'}
+					/>
+					<Select 
+						options={yearList === 'loading' ? [] : yearList}
+						isLoading={yearList === 'loading'}
+						loadingMessage={() => 'Carregando...'}
+						value={year}
+						onChange={(value) => changeVehicleData('Year', value)}
+						noOptionsMessage={() => 'Não há opções a serem selecionadas.'}
+						placeholder={yearList.length ? 'Selecione o ano do seu veículo' : 'Selecione o modelo do veículo para escolher'}
+					/>
+				</div>
+				{ fipeInfo && 
+					<div>
+						<h1>Marca: {brand.label}</h1>
+						<h2>Modelo: {model.label}</h2>
+						<h2>Ano: {year.label}</h2>
+						<h3>Valor (R$): {fipeInfo.Valor}</h3>
+						<h3>Código FIPE: {fipeInfo.CodigoFipe}</h3>
+						<h3>Combustível: {fipeInfo.Combustivel}</h3>
+						<h3>Referência: {fipeInfo.MesReferencia}</h3>
+					</div>
+				}
+				
 			</div>
 		);
 	}
--- src/stores/VehicleStore.js
+++ src/stores/VehicleStore.js
@@ -9,33 +9,119 @@ class VehicleStore {
 	}
 
 	setInitialState = () => {
-		this.type = { value: 'carro', label: 'Carro' };
+		this.type = null;
 		this.brand = null;
 		this.model = null;
 		this.year = null;
 		this.fipeInfo = null;
-		this.brandsList = [];
-		this.modelsList = [];
-		this.yearsList = [];
+		this.typeList = [
+			{ label: 'Carros', value: 'carros'}, 
+			{ label: 'Motocicletas', value: 'motos' }, 
+			{ label: 'Caminhões', value: 'caminhoes' }
+		];
+		this.brandList = [];
+		this.modelList = [];
+		this.yearList = [];
 	}
 
-	setFactory = () => {
-		this.factory = 'loading';
-		RequestProvider.get('carros/marcas')
+	changeVehicleData = (field, value) => {
+		this[field.toLowerCase()] = value;
+		this[`_onChange${field}`]();
+	}
+
+	_onChangeType = () => {
+		this._resetData(['brand', 'model', 'year', 'fipeInfo']);
+		this.brandList = 'loading';
+		RequestProvider.get(`${this.type.value}/marcas`)
 		.then(response => {
 			runInAction(() => {
-				this.factory = 'Ferrari'
-			});
+				this.brandList = responseToOptions(response);
+			})
 		})
 		.catch(err => {
 			console.log(err);
+			runInAction(() => {
+				this.brandList = new Error();
+			})
 		})
+	}
+
+	_onChangeBrand = () => {
+		this._resetData(['model', 'year', 'fipeInfo']);
+		this.modelList = 'loading';
+		RequestProvider.get(`${this.type.value}/marcas/${this.brand.value}/modelos`)
+		.then(response => {
+			runInAction(() => {
+				this.modelList = responseToOptions(response.modelos);
+			})
+		})
+		.catch(err => {
+			console.log(err);
+			runInAction(() => {
+				this.modelList = new Error();
+			})
+		})
+	}
+
+	_onChangeModel = () => {
+		this._resetData(['year', 'fipeInfo']);
+		this.yearList = 'loading';
+		RequestProvider.get(`${this.type.value}/marcas/${this.brand.value}/modelos/${this.model.value}/anos`)
+		.then(response => {
+			runInAction(() => {
+				this.yearList = responseToOptions(response);
+			})
+		})
+		.catch(err => {
+			console.log(err);
+			runInAction(() => {
+				this.yearList = new Error();
+			})
+		})
+	}
+
+	_onChangeYear = () => {
+		this._resetData(['fipeInfo']);
+		this.fipeInfo = 'loading';
+		RequestProvider.get(`${this.type.value}/marcas/${this.brand.value}/modelos/${this.model.value}/anos/${this.year.value}`)
+		.then(response => {
+			runInAction(() => {
+				this.fipeInfo = response;
+			})
+		})
+		.catch(err => {
+			console.log(err);
+			runInAction(() => {
+				this.yearList = new Error();
+			})
+		})
+	}
+
+	_resetData = (fields) => {
+		fields.forEach(field => {
+			this[field] = null;
+			this[`${field}List`] = [];
+		});
 	}
 }
 
+function responseToOptions(response) {
+	return response.map(option => {
+		return { label: option.nome, value: option.codigo };
+	})
+}
+
 export default decorate(VehicleStore, {
+	changeVehicleData: action,
 	setInitialState: action,
-	setFactory: action,
-	factory: observable,
+	typeList: observable,
+	brandList: observable,
+	modelList: observable,
+	yearList: observable,
+	type: observable,
+	brand: observable,
+	model: observable,
+	year: observable,
+	fipeInfo: observable,
 
 });
